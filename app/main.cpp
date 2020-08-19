bd881ab37c47072cc018d3dd98b3271c5b2cc7e1
Phillip Cloud <cloud@standard.ai>
2020-08-18 20:20:10 -0400

    Auto

--- app/main.cpp
+++ app/main.cpp
@@ -1,17 +1,8 @@
-#include <iostream>
-#include <algorithm>
-#include <chrono>  // NOLINT
-#include <iostream>
-#include <memory>
-#include <ostream>
-#include <string>
-
 #include "edgetpu.h"
 #include "tensorflow/lite/interpreter.h"
 #include "tensorflow/lite/model.h"
 
 int main(int argc, const char* argv[]) {
-  std::unique_ptr<tflite::FlatBufferModel> model =
-          tflite::FlatBufferModel::BuildFromFile("./foo.tflite");
+  auto model = tflite::FlatBufferModel::BuildFromFile("./foo.tflite");
   return 0;
 }
