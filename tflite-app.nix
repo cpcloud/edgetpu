aa3ee7a676190ea166992ad97abcfb471dd9ac8b
Phillip Cloud <cloud@standard.ai>
2020-08-25 11:28:39 -0400

    Remove unused libs

--- tflite-app.nix
+++ tflite-app.nix
@@ -17,34 +17,22 @@ stdenv.mkDerivation {
     libedgetpu.basic.engine
     libedgetpu.basic.engine-native
     libedgetpu.basic.resource-manager
-    libedgetpu.posenet.decoder-tflite-plugin
     libedgetpu.utils.error-reporter
     abseil-cpp
     tensorflow-lite
     flatbuffers
-    libusb
     boost
   ];
   dontConfigure = true;
   buildPhase = ''
     $CXX \
-      -fPIC \
       -o tflite-app \
-      -g \
+      -flto \
+      -O3 \
       main.cpp \
       -ledgetpu \
       -ledgetpu_basic_engine \
       -ledgetpu_basic_engine_native \
-      -ledgetpu_resource_manager \
-      -ledgetpu_posenet_decoder_tflite_plugin \
-      -ledgetpu_error_reporter \
-      -lusb-1.0 \
-      -lrt \
-      -lpthread \
-      -ldl \
-      -Wl,--whole-archive \
-      -ltensorflow-lite \
-      -Wl,--no-whole-archive \
       -lboost_program_options
   '';
   installPhase = ''
