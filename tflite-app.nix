d402c8beddaefe1ce9752c16b6113f030268dfd1
Phillip Cloud <cpcloud@gmail.com>
2021-06-13 09:45:39 -0400

    Add meson build

--- tflite-app.nix
+++ tflite-app.nix
@@ -1,69 +1,39 @@
-{ llvmPackages_latest
-, autoPatchelfHook
-, tensorflow-lite
-, flatbuffers
+{ abseil-cpp
 , boost
-, libedgetpu
-, abseil-cpp
-, xtensor
-, opencv4
-, pkgconfig
-, gst_all_1
+, cmake
+, flatbuffers
 , gobject-introspection
+, libedgetpu
+, meson
+, opencv4
+, pkg-config
+, stdenv
+, tensorflow-lite
+, xtensor
+, ninja
+, cli11
 }:
-let
-  pname = "tflite-app";
-in
 stdenv.mkDerivation {
-  inherit pname;
+  inherit "tflite-app";
   version = "1.0.0";
-  nativeBuildInputs = [ meson ];
+
+  nativeBuildInputs = [
+    meson
+    cmake
+    pkg-config
+    ninja
+  ];
+
   buildInputs = [
+    libedgetpu
     abseil-cpp
+    cli11
     tensorflow-lite
     flatbuffers
     boost
     xtensor
     opencv4
-    gst_all_1.gst-plugins-bad
-    gst_all_1.gst-plugins-base
-    gst_all_1.gst-plugins-good
-    gst_all_1.gst-plugins-ugly
-    gst_all_1.gstreamer
-    gobject-introspection
-  ] ++ (
-    with libedgetpu; [
-      max
-      dev
-      basic.engine
-      basic.engine-native
-      posenet.decoder-op
-      basic.resource-manager
-      utils.error-reporter
-    ]
-  );
-  # dontConfigure = true;
-  # buildPhase = ''
-  #   $CXX \
-  #     -o ${pname} \
-  #     -O3 \
-  #     -std=c++17 \
-  #     main.cpp \
-  #     -pthread \
-  #     $(pkg-config --cflags opencv4) \
-  #     $(pkg-config --libs opencv4) \
-  #     -lboost_program_options \
-  #     -ledgetpu \
-  #     -ledgetpu_basic_engine \
-  #     -ledgetpu_basic_engine_native \
-  #     -ltensorflow-lite \
-  #     -lrt \
-  #     -ldl
-  # '';
-  # installPhase = ''
-  #   mkdir -p "$out/bin"
-  #   install ${pname} $out/bin
-  # '';
+  ];
 
   src = ./app;
 }
