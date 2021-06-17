6f4615530890e49ad5560b28e231d7f907736fb0
Phillip Cloud <cpcloud@gmail.com>
2021-06-17 14:20:00 -0400

    Use more iterators

--- src/engine.rs
+++ src/engine.rs
@@ -3,6 +3,7 @@ use crate::{
     pose::{self, Keypoint, KeypointKind, Pose},
     tflite,
 };
+use ndarray::Axis;
 use num_traits::cast::{FromPrimitive, ToPrimitive};
 use opencv::{core::Mat, prelude::*};
 use std::{
@@ -26,8 +27,9 @@ impl Engine {
         P: AsRef<Path>,
     {
         let mut interpreter = tflite::Interpreter::new(model_path)?;
-        interpreter.allocate_tensors()?;
         assert_eq!(interpreter.get_output_tensor_count(), 4);
+
+        interpreter.allocate_tensors()?;
         Ok(Self {
             interpreter,
             timing: Default::default(),
@@ -63,11 +65,14 @@ impl Engine {
             pose_keypoints.dim(2),
             pose_keypoints.dim(3),
         ))?;
+
         let keypoint_scores = self.interpreter.get_output_tensor(1)?;
         let keypoint_scores =
             keypoint_scores.as_ndarray((keypoint_scores.dim(1), keypoint_scores.dim(2)))?;
+
         let pose_scores = self.interpreter.get_output_tensor(2)?;
         let pose_scores = pose_scores.as_ndarray(pose_scores.dim(1))?;
+
         let nposes = self.interpreter.get_output_tensor(3)?.as_slice()[0]
             .to_usize()
             .ok_or(Error::ConvertNumPosesToUSize)?;
@@ -75,11 +80,15 @@ impl Engine {
         // move poses into a more useful structure
         let mut poses = Vec::with_capacity(nposes);
 
-        for (pose_i, keypoint) in pose_keypoints.axis_iter(ndarray::Axis(0)).enumerate() {
-            let mut keypoint_map: pose::Keypoints = Default::default();
+        for (pose_i, (keypoint, &pose_score)) in pose_keypoints
+            .axis_iter(Axis(0))
+            .zip(pose_scores)
+            .enumerate()
+        {
+            let mut keypoints: pose::Keypoints = Default::default();
 
-            for (point_i, point) in keypoint.axis_iter(ndarray::Axis(0)).enumerate() {
-                keypoint_map[point_i] = Keypoint {
+            for (point_i, point) in keypoint.axis_iter(Axis(0)).enumerate() {
+                keypoints[point_i] = Keypoint {
                     kind: Some(
                         KeypointKind::from_usize(point_i)
                             .ok_or(Error::ConvertUSizeToKeypointKind(point_i))?,
@@ -90,8 +99,8 @@ impl Engine {
             }
 
             poses.push(Pose {
-                keypoints: keypoint_map,
-                score: pose_scores[pose_i],
+                keypoints,
+                score: pose_score,
             });
         }
 
